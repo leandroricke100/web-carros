88bc9722cfa2c0f2f2fa61721aeb24f155bdebb0
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-11-20 14:58:39 -0300

    criando as paginas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,15 +7,27 @@ export function Header() {
   const loadingAuth = false;
 
   return (
-    <div>
-      <header>
+    <div className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4 ">
+      <header className="flex w-full items-center justify-between max-w-7xl px-4 mx-auto">
         <Link to="/">
           <img src={logoImg} alt="logo web carros" />
         </Link>
 
-        <Link to="/dashboard">
-          <FiUser size={24} color="#000" />
-        </Link>
+        {!loadingAuth && signed && (
+          <Link to="/dashboard">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiUser size={22} color="#000" />
+            </div>
+          </Link>
+        )}
+
+        {!loadingAuth && !signed && (
+          <Link to="/login">
+            <div className="border-2 rounded-full p-1 border-gray-900">
+              <FiLogIn size={22} color="#000" />
+            </div>
+          </Link>
+        )}
       </header>
     </div>
   );
